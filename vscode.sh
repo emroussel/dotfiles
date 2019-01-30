fac715babf78792530dbb30ef681ae7946fc9366
Emmanuel Roussel <emmanuel@emroussel.xyz>
2019-01-29 21:59:51 -0500

    Remove prettier standard from vscode extensions

--- vscode.sh
+++ vscode.sh
@@ -22,7 +22,6 @@ declare -a EXTENSIONS=(
   formulahendry.auto-close-tag  # Automatically add HTML/XML close tag
   formulahendry.auto-rename-tag  # Auto rename paired HTML/XML tag
   kumar-harsh.graphql-for-vscode # GraphQL syntax highlighting, linting, and auto-complete
-  numso.prettier-standard-vscode # Prettier formatter with Standard rules
   octref.vetur # Vue tooling
   robinbentley.sass-indented # Sass syntax highlighting, auto-complete and snippets
   wayou.vscode-todo-highlight # Comment keyword highlighting
