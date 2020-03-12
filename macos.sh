ce201525e60e0fc62a2eab7dcb0af59e21f7b5a1
Emmanuel Roussel <emmanuel@emroussel.xyz>
2020-03-12 10:21:52 -0400

    Disable showing screenshot thumbnails

--- macos.sh
+++ macos.sh
@@ -74,6 +74,9 @@ defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
+# Don't show screenshot thumbnails
+defaults write com.apple.screencapture show-thumbnail -bool FALSE
+
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
